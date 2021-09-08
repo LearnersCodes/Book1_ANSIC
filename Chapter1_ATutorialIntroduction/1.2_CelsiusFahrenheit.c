0b7c7e8d9f771dd4ead83b3a97c604f3af038011
dhatri <dhatri.mathad@gmail.com>
2021-09-08 19:32:50 +0530

    Conversion from celsius back to fahrenheit.

--- Chapter1_ATutorialIntroduction/1.2_CelsiusFahrenheit.c
+++ Chapter1_ATutorialIntroduction/1.2_CelsiusFahrenheit.c
@@ -3,6 +3,8 @@ File name : 1.2_CelsiusFahrenheit.c
 Date : 8/9/2021
 Brief : Converts Fahrenheit to Celsius,
         for fahrenheit values from 0, 20, ..., 300.
+        Converts Celsius to Fahrenheit,
+        for celsuis calues from 0, 20, ..., 300.
 
 **************************************************/
 
@@ -19,11 +21,26 @@ int main (void)
 
   fahr = lower;
 
+  printf("Fahrenheit to Celsius Conversion:\n");
+  printf("Fahrenheit     Celsius\n");
+
   while(fahr <= upper)
   {
     /* 5.0/9.0 is not truncated. */
     cel = (5.0/9.0) * (fahr - 32.0);
-    printf("%3.0f\t%6.1f\n", fahr, cel);
+    printf("%10.0f\t%6.1f\n", fahr, cel);
     fahr = fahr + step;
+  }
+
+  printf("\nCelsius to Fahrenheit Conversion:\n");
+  printf("Celsius     Fahrenheit\n");
+
+  cel = lower;
+  while(cel <= upper)
+  {
+    /* 5.0/9.0 is not truncated. */
+    fahr = (cel * 9.0/5.0) + 32.0;
+    printf("%7.0f\t%14.1f\n", cel, fahr);
+    cel = cel + step;
   }
 }
