ce7730535bdf6b97011ca56ad2ec3e9e5c8e512a
dhatri <dhatri.mathad@gmail.com>
2021-09-13 15:41:10 +0530

    Calculate powers using a function.

---
+++ Chapter1_ATutorialIntroduction/1.7_PowerFunc.c
@@ -0,0 +1,34 @@
+/**********************************************************
+ File name : 1.7_PowerFunc.c
+ Date : 13 SEP 2021
+ Brief : Function to calculate power of a base.
+
+**********************************************************/
+
+#include <stdio.h>
+
+int power (int base, int pow);
+
+int main(void)
+{
+  int i;
+
+  printf ("Power 2Powers 3Powers\n");
+  for (i=0; i<10; i++)
+  {
+    printf("%5d %7d %7d\n", i, power(2,i), power(3,i));
+  }
+  return 0;
+}
+
+int power (int base, int pow)
+{
+  int i,p;
+  p = 1;
+
+  for (i=1; i<=pow; i++)
+  {
+    p = p * base;
+  }
+  return p;
+}
